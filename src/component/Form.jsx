44dd8e4c76f01ef8bca72eacf58d05667b216174
arpitaumath <arpitaumath@gmail.com>
2025-02-09 20:22:02 +0530

    inputtypechange

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -23,7 +23,7 @@ const Form = () => {
        <div className=' w-full  flex flex-col items-center justify-center text-white pt-40 pb-10'>
             <h1 className='w-3/4  text-6xl font-black text-center '>Shorten Your Big URL With Our Tool</h1>
             <form className='w-3/4 mt-10' onSubmit={handleSubmit}>
-              <input className='h-14 w-2/3 rounded-l-md outline-none text-black px-5 placeholder:text-black' placeholder='Enter the link ' onChange={(e) => setText(e.target.value)} value={text}     />
+              <input className='h-14 w-2/3 rounded-l-md outline-none text-black px-5 placeholder:text-black'  type='url' placeholder='Enter the link ' onChange={(e) => setText(e.target.value)} value={text}     />
               <button className='h-14 pb-1  w-1/3 rounded-r-md text-xl font-bold bg-green-700'>Shorter</button>
             </form>
           </div>
